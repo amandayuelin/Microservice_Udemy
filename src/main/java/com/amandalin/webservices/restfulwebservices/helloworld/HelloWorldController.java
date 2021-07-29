59fb5f5771fdd2760b619070231876fd7905b92d
amanda <linyue@linyuedeMacBook-Pro.local>
2021-07-29 16:27:02 -0700

    step 36

--- src/main/java/com/amandalin/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/amandalin/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,18 @@
 package com.amandalin.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     //GET
     //URI - /hello-world
     //method - "Hello World"
@@ -19,5 +24,17 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message",null, "Default Message", locale);
+
+
     }
 }
--- src/main/java/com/amandalin/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/amandalin/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,21 @@
 package com.amandalin.webservices.restfulwebservices.user;
 
+import com.amandalin.webservices.restfulwebservices.exception.CustomizedResponseEntityExceptionHandler;
+import com.amandalin.webservices.restfulwebservices.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -22,15 +31,22 @@ public class UserResource {
 
     //retrieveUser(int id)
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+    public EntityModel<User> retrieveUser(@PathVariable int id) throws Exception {
+        User user = service.findOne(id);
+        if(user == null) {
+            throw new UserNotFoundException("id = " + id);
+        }
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
     }
 
 
     //input - details of user
     //output - Created & return the created URI
     @PostMapping("/users")
-    public ResponseEntity<Object> createdUser(@RequestBody User user) {
+    public ResponseEntity<Object> createdUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         //Created
         // /users/{id}
@@ -39,6 +55,14 @@ public class UserResource {
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if(user == null) {
+            throw new UserNotFoundException("id = " + id);
+        }
     }
 
 
