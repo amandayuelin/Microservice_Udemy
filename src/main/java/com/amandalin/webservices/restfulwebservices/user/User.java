e2e8ac1847a3989586724f7f6dbac579db63f6b1
amanda <linyue@linyuedeMacBook-Pro.local>
2021-07-30 11:17:44 -0700

    step 46

--- src/main/java/com/amandalin/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/amandalin/webservices/restfulwebservices/user/User.java
@@ -1,9 +1,16 @@
 package com.amandalin.webservices.restfulwebservices.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters.")
